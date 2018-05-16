179535c9a57b034031f68989a8be1b38517bf7cf
Jonathan Manning <jmanning@ebi.ac.uk>
2018-05-16 08:48:38 +0100

    Allow download to flat output location rather than ENA-like substructure

--- fetchFASTQFromENA.sh
+++ fetchFASTQFromENA.sh
@@ -12,22 +12,28 @@ fi
 
 library=$1
 outputDirectory=$2
+enaStructureAtOutput=${3:-yes}
 
 check_variables "library" "outputDirectory"
 
-# Construct library paths for remote and local
+# Get the ENA-like subdirectory structure
 
 subDir=`get_library_subdir $library`
 
 # Create local directory for FASTQ file - if it doesn't already exist
 
-localFastqDir=${outputDirectory}/$subDir
+if [ $enaStructureAtOutput = 'yes' ]; then
+    localFastqDir=${outputDirectory}/$subDir
+else
+    localFastqDir=${outputDirectory}
+fi
+
 mkdir -p $localFastqDir
-chmod -R g+w $(dirname $outputDirectory)
+chmod -R g+w $localFastqDir
 
 # Get all files for the library with rsync
 
-`fetch_ena_sudo_string` rsync -ssh -avh --no-p ${ENA_NODE}:${ENA_ROOT_DIR}/${subDir}/ ${outputDirectory}/$subDir > /dev/null 2>&1
+`fetch_ena_sudo_string` rsync -ssh -avh --no-p ${ENA_NODE}:${ENA_ROOT_DIR}/${subDir}/ $localFastqDir > /dev/null 2>&1
 errCode=$?
 
 if [ $errCode -ne 0 ]; then 
